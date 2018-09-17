70e309a38a442770778faba4f635aa1d446aa9b1
Soto <soto.ericf@gmail.com>
2018-09-17 14:00:55 -0700

    Displaying tasks

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,16 +6,27 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
+  tasks = {};
 
   constructor(private _httpService: HttpService){
   }
-
+  ngOnInit(){
+    this.getTasksFromService()
+  }
   GetAllTasks(){
     return this._httpService.getTasks();
   }
   GetOneTask(){
     return this._httpService.getOneTask();
+  }
+  getTasksFromService(){
+    let observable = this._httpService.getTasksFromService()
+    observable.subscribe(data => {
+      console.log("Got our data!", data)
+      this.tasks = data;
+      console.log(this.tasks)
+    })
   }
 }
